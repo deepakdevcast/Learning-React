8e3c1197f9865699ab4d6291aa4d6d47f71c91ad
deepakjuit <dk8232847@gmail.com>
2021-10-16 18:51:53 +0530

    class component work

--- workingwithreact/src/App.js
+++ workingwithreact/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {Welcome} from './Components/Welcome';
+import Hello from './Components/Hello';
 function App() {
   return (
     <div className="App">
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
